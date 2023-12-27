0a8a81261b9702fdb34076a7fbdf98cf078970b4
MostafaZayed72 <zayed8646@gmail.com>
2023-12-27 17:26:10 +0200

    eddit size of image

--- src/components/home_page/TopCategories.vue
+++ src/components/home_page/TopCategories.vue
@@ -7,57 +7,57 @@ const categories = ref([
       "https://new-ella-demo.myshopify.com/cdn/shop/files/mobile-phones.png?v=10616357514144629263",
   },
   {
-    title: "Mobile Phones",
+    title: "LabTops",
     image:
       "https://new-ella-demo.myshopify.com/cdn/shop/files/laptops-tablets.png?v=17546078654541140209",
   },
   {
-    title: "Mobile Phones",
+    title: "TVS",
     image:
       "https://new-ella-demo.myshopify.com/cdn/shop/files/televisions.png?v=792225267698738137",
   },
   {
-    title: "Mobile Phones",
+    title: "PCs",
     image:
       "https://new-ella-demo.myshopify.com/cdn/shop/files/pcs-accessories.png?v=7913320403362682840",
   },
   {
-    title: "Mobile Phones",
+    title: "Audio & Video",
     image:
       "https://new-ella-demo.myshopify.com/cdn/shop/files/audio-video.png?v=17118273463527967791",
   },
   {
-    title: "Mobile Phones",
+    title: "Gaming",
     image:
       "https://new-ella-demo.myshopify.com/cdn/shop/files/gaming-accessories.png?v=5365032838061682635",
   },
   {
-    title: "Mobile Phones",
+    title: "Home Appliances",
     image:
       "https://new-ella-demo.myshopify.com/cdn/shop/files/home-appliances.png?v=4122646624966862882",
   },
   {
-    title: "Mobile Phones",
+    title: "Kitchen",
     image:
       "https://new-ella-demo.myshopify.com/cdn/shop/files/kitchen-appliances.png?v=912885117927234410",
   },
   {
-    title: "Mobile Phones",
+    title: "Cameras",
     image:
       "https://new-ella-demo.myshopify.com/cdn/shop/files/cameras.png?v=10468174360605840515",
   },
   {
-    title: "Mobile Phones",
+    title: "Consumables",
     image:
       "https://new-ella-demo.myshopify.com/cdn/shop/files/consumables.png?v=1604898607781595964",
   },
   {
-    title: "Mobile Phones",
+    title: "Smart Homes",
     image:
       "https://new-ella-demo.myshopify.com/cdn/shop/files/smart-homes.png?v=11511407578445752828",
   },
   {
-    title: "Mobile Phones",
+    title: "Accessories",
     image:
       "https://new-ella-demo.myshopify.com/cdn/shop/files/accessories.png?v=16090558546215869367",
   },
@@ -88,7 +88,7 @@ const categories = ref([
                   class="image-parent"
                   style="
                     overflow: hidden;
-                    width: 100%;
+                    width: 170px;
                     height: 170px;
                     border-radius: 50%;
                   "
---
+++ src/components/home_page/whyShopWithUs.vue
@@ -0,0 +1,1 @@
+<template><div class="shop-with-us"></div></template>
