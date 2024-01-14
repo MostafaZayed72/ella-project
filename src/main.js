30b40adc9b7c9c942a8af8ba94705d8a0c1e4caa
MostafaZayed72 <zayed8646@gmail.com>
2024-01-15 00:58:50 +0200

    nu

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import "./index.css";
 
 //Pinia config
 import { createPinia } from "pinia";
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
-import path from "path"
+import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
-    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
+    extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json", ".vue"],
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
